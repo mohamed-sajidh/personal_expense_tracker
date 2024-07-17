d59fa841b6ef4c929be0198911e2bd34750eb54d
sajidh <sajidhshaji4@gmail.com>
2024-07-17 09:29:24 +0530

    folder structure completed

---
+++ lib/presentation/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 40,
+        width: 30,
+        color: Colors.red,
+      ),
+    );
+  }
+}
